0368ca1441d88390803baad14bb622845ffeadb7
Roberto Serrano <robertoserrano83@gmail.com>
2012-12-09 20:26:06 +0100

    Update backupTool.sh

--- backupTool.sh
+++ backupTool.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 #**********************************************#
-#		      backupTool.sh                    #
-#	        Written by gdsr                    #
-#		     Dec 06, 2012					   #
+#		      backupTool.sh            #
+#	        Written by gdsr                #
+#		     Dec 06, 2012	       #
 #            Version: 1.0                      #
-#		<obertoserrano83@gmail.com>		       #
-#	    Script que realiza backup totales	   #
-#         y diferenciales automaticamente. 	   #
-#             Script parateters:			   #
-#				-h 		help				   #
-# 				-c    Backups  			   	   #	
+#		<obertoserrano83@gmail.com>    #
+#	    Script que realiza backup totales  #
+#         y diferenciales automaticamente.     #
+#             Script parateters:               #
+#		-h	help		       #
+# 		-c    	Backups  	       #	
 #**********************************************#
 # Only user root
 
 # NOT modify these variables
-ROOT_UID=0							# Only user with $UID 0 have root privileges.
-E_NOTROOT=87						# Non-root error.
-E_WRONG_ARGS=85						# Not parameters error.
-SCRIPT_PARAMETERS="-c -h -t"		# Script-parameters.
-DDAY=`date +%d`						# Current day
-BZ2="jcvf" 							# Compression bz2
+ROOT_UID=0					# Only user with $UID 0 have root privileges.
+E_NOTROOT=87					# Non-root error.
+E_WRONG_ARGS=85					# Not parameters error.
+SCRIPT_PARAMETERS="-c -h -t"			# Script-parameters.
+DDAY=`date +%d`					# Current day
+BZ2="jcvf" 					# Compression bz2
 # These variables can be modified
-NOM_FILE="home-bin-"					# Termination file name ej: BackT-home-date
+NOM_FILE="home-bin-"				# Termination file name ej: BackT-home-date
 DIR_SAVE="/home/roberto/bin"			# Directorios a realizar backups
 DIR_SAVE_LOG="/home/roberto/"			# Directorio destino archivo log
 DIR_SAVE_BACKT=/home/roberto/			# Directorio destino del backup
-DDAYT="01"								# Dia del mes de la copia total
+DDAYT="01"					# Dia del mes de la copia total
 # Declaro array con los directorios a copiar y nombres de archivo
 # Arrays no modificar
 ARRAY=( $DIR_SAVE )
