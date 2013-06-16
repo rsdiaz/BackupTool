a5b3baedc786f04edac936aba73294b1a09c2f68
Roberto Serrano <robertoserrano83@gmail.com>
2013-06-17 00:53:15 +0200

    Update backupTool.sh

--- backupTool.sh
+++ backupTool.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-#**********************************************#
-#		 backupTool.sh                 #
-#	            by gdsr                    #
-#		  Dec 06, 2012	               #
-#                 Version: 1.0                 #
-#	     <robertoserrano83@gmail.com>      #
-#	  Script que realiza backup totales    #
-#         y diferenciales automaticamente.     #
-#             Parametros del script            #
-#		-h	help		       #
-# 		-c    	Backups total y        #
-#			diferencial automatico #
-#		-t	Backup total unico     #
-#					       #	
-#**********************************************#
+#***************************************************************#
+#		       backupTool.sh 				#
+#			Version 1.0				#
+#		<robertoserrano83@gmail.com>			#
+#	           Copyright 2012 robfree            		#
+#    This program is free software: you can redistribute 	#
+#         it and/or modify it under the terms of the 		#
+#         GNU General Public License as published by		#
+#         the Free Software Foundation, either version 3 	#
+#    of the License, or (at your option) any later version.	#
+#								#
+#    This program is distributed in the hope that it will 	#
+#    be useful, but WITHOUT ANY WARRANTY; without even the	#
+#    implied warranty of MERCHANTABILITY or FITNESS FOR A 	#
+#    PARTICULAR PURPOSE.  See the GNU General Public License	#
+#    for more details.						#
+#								#
+#          You should have received a copy of the 		#
+#    GNU General Public License along with this program.	#
+#         If not, see <http://www.gnu.org/licenses/>.	        #
+#								#
+#	  Script que realiza backup totales    			#
+#         y diferenciales automaticamente.     			#
+#             Parametros del script            			#
+#		-h	help		       			#
+# 		-c    	Backups total y        			#
+#			diferencial automatico 			#
+#		-t	Backup total unico     			#
+#					       			#	
+#***************************************************************#
 
 # NO modificar variables
 
